cf5022d784de1f58339f2f6716754f03fd8dfe4f
Ben <cookbp@gmail.com>
2021-12-05 11:08:20 -0500

    day3 complete

--- src/day3.rs
+++ src/day3.rs
@@ -1,142 +1,86 @@
 use std::cmp::Ordering;
 
-#[derive(PartialEq, Eq, Debug)]
-pub struct BitCounter {
-    zeros: u32,
-    ones: u32
+fn binary_string_to_int(bits: &str) -> u64 {
+    bits.bytes()
+        .rev()
+        .enumerate()
+        .filter( |(_,bit)| *bit==b'1')
+        .map( |(i,_)| 1u64 << i)
+        .sum()
 }
-
-impl BitCounter {
-    fn new() -> Self {
-        Self {zeros:0, ones:0}
-    }
-}
-
-fn count_bits(input:&[Vec<u8>]) -> Vec<BitCounter> {
-    let n_bits = input[0].len();
-    let mut counters = Vec::<BitCounter>::new();
-    for _ in 0..n_bits {counters.push(BitCounter::new())}
-    for num in input {
-        for (counter,bit) in counters.iter_mut().zip(num.iter()) {
-            match bit {
-                b'0' => counter.zeros += 1,
-                b'1' => counter.ones += 1,
-                _ => panic!("invalid input character")
-            }
-        }
-    }
-    counters
-}
-
-// #[aoc_generator(day3)]
-// pub fn count_bits(input: &str) -> Vec<BitCounter> {
-//     let mut lines = input.lines().peekable();
-//     let n_bits = lines.peek().unwrap().len();
-//     let mut counters = Vec::<BitCounter>::new();
-//     for _ in 0..n_bits {counters.push(BitCounter::new())}
-
-//     for line in lines {
-//         for (counter, bit) in counters.iter_mut().zip(line.bytes()) {
-//             match bit {
-//                 b'0' => counter.zeros += 1,
-//                 b'1' => counter.ones += 1,
-//                 _ => panic!("invalid input character")
-//             }
-//         } 
-//     }
-//     counters
-// }
 
 #[aoc_generator(day3)]
-pub fn parse_part2(input: &str) -> Vec<Vec<u8>> {
-    input.lines().map(|num| num.as_bytes().to_vec()).collect()
+fn parse(input: &str) -> Vec<u64> {
+    input.lines()
+        .map(binary_string_to_int)
+        .collect()
 }
 
+fn most_frequent_value(numbers: &[u64], position: u8) -> u64 {
+    let n = numbers.len();
+    let ones = numbers.iter()
+        .filter(|x| *x & (1u64 << position) > 0)
+        .count();
+    match ones.cmp(&(n-ones)) {
+        Ordering::Greater => 1,
+        Ordering::Equal => 1,
+        Ordering::Less => 0
+    }
+}
 
-#[aoc(day3, part1)]
-pub fn solve_part1(input: &[Vec<u8>]) -> u32 {
-    let input = count_bits(input);
-// pub fn solve_part1(input: &[BitCounter]) -> u32 {
-    let mut gamma: u32 = 0;
-    let mut epsilon: u32 = 0;
-    for (pos,counts) in input.iter().rev().enumerate() {
-        match counts.ones.cmp(&counts.zeros) {
-            Ordering::Greater => {
-                gamma += 1u32 << pos;
-            },
-            Ordering::Less => {
-                epsilon += 1u32 << pos;
-            },
-            Ordering::Equal => panic!("Indeterminate comparison")
+fn get_num_bits(n: u64) -> u8 {
+    for i in 0..64u8 {
+        if n >> i == 0 {return i}
+    }
+    unreachable!()
+}
+
+fn find_single_number(mut numbers: Vec<u64>, invert: bool) -> u64 {
+    let n_bits: u8 = get_num_bits(*numbers.iter().max().unwrap());
+    let mut val: u64;
+    for i in (0..n_bits).rev() {
+        val = most_frequent_value(&numbers, i);
+        if invert {val = 1 - val};
+        numbers = numbers.into_iter().filter(|x| (x >> i) % 2 == val).collect();
+        match numbers.len() {
+            1 => return numbers[0],
+            0 => panic!("All numbers eliminated"),
+            _ => ()
         }
+    }
+    panic!("Exhausted all bits")
+}
+
+#[aoc(day3,part1)]
+pub fn solve_part1(input: &Vec<u64>) -> u64 {
+    let n_bits: u8 = get_num_bits(*input.iter().max().unwrap());
+    let mut gamma = 0u64; 
+    let mut epsilon = 0u64;
+    let mut val: u64;
+    for i in 0..n_bits {
+        val = most_frequent_value(input, i);
+        gamma += val << i;
+        epsilon += (1-val) << i;
     }
     gamma * epsilon
 }
 
-#[derive(Copy, Clone)]
-enum Criterion {
-    Oxygen,
-    CO2,
-}
-
-fn filter_numbers<'a>(input: &[&'a [u8]], bit: usize, criterion: Criterion) -> Vec<&'a [u8]> {
-    let mut counter = BitCounter::new();
-    input.iter().for_each(|num| match num[bit] {
-        b'0' => counter.zeros += 1,
-        b'1' => counter.ones += 1,
-        _ => panic!("invalid input bit")
-    });
-    let keep_bit = match (criterion, counter.ones.cmp(&counter.zeros)) {
-        (Criterion::Oxygen, Ordering::Greater) => b'1',
-        (Criterion::Oxygen, Ordering::Equal) => b'1',
-        (Criterion::Oxygen, Ordering::Less) => b'0',
-        (Criterion::CO2, Ordering::Greater) => b'0',
-        (Criterion::CO2, Ordering::Equal) => b'0',
-        (Criterion::CO2, Ordering::Less) => b'1',
-    };
-
-    let mut result = Vec::<&[u8]>::new();
-
-    input.iter()
-        .filter(|num| num[bit] == keep_bit)
-        .for_each(|num| result.push(&num));
-
-    result
-}
-
-// input could also be mutable (clone before passing)
-fn iterated_filter(input: &[&[u8]], criterion: Criterion) -> Vec<u8> {
-    let n_bits: usize = input[0].len();
-    let mut input: Vec<&[u8]> = input.to_vec();
-    for bit in 0..n_bits {
-        input = filter_numbers(&input, bit, criterion);
-        match input.len() {
-            0 => panic!("No numbers remaining"),
-            1 => return input[0].to_vec(),
-            _ => ()
-        }
-    }
-    panic!("Exhausted all filters")
-}
-
-
 #[aoc(day3, part2)]
-pub fn solve_part2(input: &[Vec<u8>]) -> u32 {
-    // let input: Vec<&[u8]> = input.lines()
-    //     .map(|num| num.as_bytes())
-    //     .collect();
-    0
-    
+pub fn solve_part2(input: &Vec<u64>) -> u64 {
+    let gamma = find_single_number(input.clone(), false);
+    let epsilon = find_single_number(input.clone(), true);
+    gamma * epsilon
 }
 
 #[cfg(test)]
 mod tests {
     use super::{
-        BitCounter, count_bits, solve_part1,
-        parse_part2,
-        filter_numbers, Criterion, iterated_filter
+        parse,
+        most_frequent_value,
+        find_single_number,
+        solve_part1,
+        solve_part2
     };
-
     const INPUT: &str = "00100
 11110
 10110
@@ -149,46 +93,38 @@ mod tests {
 11001
 00010
 01010";
-    const COUNTS: [BitCounter; 5] = [
-        BitCounter {zeros:5, ones:7},
-        BitCounter {zeros:7, ones:5},
-        BitCounter {zeros:4, ones:8},
-        BitCounter {zeros:5, ones:7},
-        BitCounter {zeros:7, ones:5}
+    
+    const NUMBERS: [u64;12] = [
+        4,30,22,23,21,15,7,28,16,25,2,10
     ];
 
     #[test]
-    fn test_input() {
-        assert_eq!(count_bits(&parse_part2(INPUT)), COUNTS);
+    fn test_parse() {
+        assert_eq!(parse(&INPUT), NUMBERS);
+    }
+
+    #[test]
+    fn test_frequent_values() {
+        assert_eq!(most_frequent_value(&NUMBERS,4),1);
+        assert_eq!(most_frequent_value(&NUMBERS,3),0);
+        assert_eq!(most_frequent_value(&NUMBERS,2),1);
+        assert_eq!(most_frequent_value(&NUMBERS,1),1);
+        assert_eq!(most_frequent_value(&NUMBERS,0),0);
     }
 
     #[test]
     fn test_part1() {
-        assert_eq!(solve_part1(&parse_part2(INPUT)), 198u32);
+        assert_eq!(solve_part1(&NUMBERS.to_vec()), 198u64);
     }
 
     #[test]
-    fn test_filter() {
-        let input: Vec<&[u8]> = INPUT.lines().map(|num| num.as_bytes()).collect();
-        let expected_result: Vec<&[u8]> = vec![
-            input[0],
-            input[5],
-            input[6],
-            input[10],
-            input[11]
-        ];
-        assert_eq!(
-            filter_numbers(&input, 0, Criterion::CO2),
-            expected_result
-        );
+    fn test_reduce() {
+        assert_eq!(find_single_number(NUMBERS.to_vec(),false), 23u64);
+        assert_eq!(find_single_number(NUMBERS.to_vec(),true), 10u64);
     }
 
     #[test]
-    fn test_iterated_filter() {
-        let input: Vec<&[u8]> = INPUT.lines().map(|num| num.as_bytes()).collect();
-        let expected_result = "10111".as_bytes().to_vec();
-        assert_eq!(iterated_filter(&input,Criterion::Oxygen), expected_result);
+    fn test_part2() {
+        assert_eq!(solve_part2(&NUMBERS.to_vec()), 230);
     }
-
-    
 }
