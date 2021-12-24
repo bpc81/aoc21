ceb75f79dd5043572d16a447137ef1edb88c5f87
Ben <cookbp@gmail.com>
2021-12-24 00:36:34 -0500

    day 6 complete

--- src/day6.rs
+++ src/day6.rs
@@ -1,4 +1,4 @@
-
+use std::collections::VecDeque;
 
 fn timestep(fish: &mut Vec<u8>) {
     let mut spawn: usize = 0;
@@ -14,6 +14,8 @@ fn timestep(fish: &mut Vec<u8>) {
     fish.resize(fish.len()+spawn, 8);
 }
 
+
+
 #[aoc(day6,part1)]
 pub fn solve_part1(input:&str) -> usize {
     let mut fish: Vec<u8> = input.split(",").map(|s| s.parse().unwrap()).collect();
@@ -23,9 +25,32 @@ pub fn solve_part1(input:&str) -> usize {
     fish.len()
 }
 
+fn update(population: &mut VecDeque<usize>) {
+    // let spawn = population.pop_front().unwrap();
+    // population.push_back(spawn);
+    // population[6] += spawn;
+    let spawn = population[0];
+    population.rotate_left(1);
+    population[6] += spawn;
+}
+
+#[aoc(day6,part2)]
+pub fn solve_part2(input: &str) -> usize {
+    // let mut population = [0usize;9];
+    let mut population = VecDeque::from([0usize;9]);
+    for s in input.split(",") {
+        population[s.parse::<usize>().unwrap()] += 1;
+    }
+    for _ in 0..256 {
+        update(&mut population);
+    }
+    population.iter().sum()
+}
+
 #[cfg(test)]
 mod tests {
-    use super::{timestep, solve_part1};
+    use super::{timestep, solve_part1, update, solve_part2};
+    use std::collections::VecDeque;
 
     #[test]
     fn test_timestep() {
@@ -42,6 +67,19 @@ mod tests {
         assert_eq!(solve_part1(&input), 5934);
     }
 
+    #[test]
+    fn test_update() {
+        let mut population: VecDeque<usize> = VecDeque::from([0,1,1,2,1]);
+        population.resize(9,0);
+        update(&mut population);
+        assert_eq!(population, VecDeque::from([1,1,2,1,0,0,0,0,0]));
+        update(&mut population);
+        assert_eq!(population, VecDeque::from([1,2,1,0,0,0,1,0,1]));
+    }
 
-
+    #[test]
+    fn test_part2() {
+        let input = "3,4,3,1,2";
+        assert_eq!(solve_part2(&input), 26984457539);
+    }
 }
